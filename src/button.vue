e12ed36e5d8bafd174a33fc83980b89925dd43a4
li_shuai <506507881@qq.com>
2019-08-05 22:07:00 +0800

    添加 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,28 +1,58 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script >
-export default {}
+export default {
+    props: ['icon','iconPosition']  // left right
+}
 </script>
 
 <style lang="scss">
-  .g-button {
-      font-size: var(--font-size);
-      height: var(--button-height);
-      padding: 0 1em;
-      border-radius: var(--border-radius);
-      border: 1px solid var(--border-color);
-      background: var(--button-bg);
-      &:hover{
-          border-color: var(--border-color-hover);
+.g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    display: inline-flex;
+    align-items: center;
+    vertical-align: middle;
+    line-height: var(--font-size);
+    &:hover{
+        border-color: var(--border-color-hover);
+    }
+    &:active{
+        background-color: var(--button-active-bg);
+    }
+    &:focus{
+        outline: none;
+    }
+    > .icon{
+        order: 1; 
+        margin-right: .3em;
+    }
+    > .content{
+        order: 2;
+    }
+    &.icon-right{
+      > .icon{
+          order: 2;
+          margin-left: .3em;
+          margin-right: 0;
       }
-      &:active{
-          background-color: var(--button-active-bg);
+      > .content {
+          order: 1;
       }
-      &:focus{
-          outline: none;
-      }
-  }
+    }
+}
 
 </style>
