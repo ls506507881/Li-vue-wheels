f3db93d8e41f956f96f62f44c7fb1f3af6b48593
li_shuai <506507881@qq.com>
2019-08-06 23:30:44 +0800

    使用 chai.expect 添加四个测试用例，第五个未完

--- src/app.js
+++ src/app.js
@@ -14,3 +14,96 @@ new Vue({
         loading3: false
     }
 })
+
+import chai from 'chai'
+
+const expect = chai.expect
+// 单元测试
+{
+    // console.log(Button) // 对象没法通过对象实例化一个东西
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon: 'settings'
+        }
+    })  //这个button 就是button实例
+    // button.$mount()
+    vm.$mount('#test')  // 通过js把按钮写到页面
+
+    // 测试
+    let useElement = vm.$el.querySelector('use')
+    // console.log(useElement)
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')  // 期待href = #i-settings
+    // 正确就不报错，不正确就报错
+    vm.$el.remove()  //button元素删掉
+    vm.$destroy()  //button对象删掉
+}
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon: 'settings',
+            loading: true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    vm.$el.remove()  //button元素删掉
+    vm.$destroy()  //button对象删掉
+}
+//检查icon位置
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'left'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()  //button元素删掉
+    vm.$destroy()  //button对象删掉
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()  //button元素删掉
+    vm.$destroy()  //button对象删掉
+}
+// 检测click
+{
+    // 怎么期待click函数被执行，使用mock
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    vm.$mount()
+    vm.$on('click',function(){
+        // console.log(1)
+        expect(1).to.eq(1)  //随便写一句永远成立的话，但是这种测试方法是错误的
+    })
+    // console.log(gButton.$el)
+    // 期望这个函数被执行
+    let button = vm.$el
+    button.click()
+}
