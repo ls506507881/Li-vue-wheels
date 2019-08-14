dda947bffc19ca22cf99f301e78c6b033c87bb8b
li_shuai <506507881@qq.com>
2019-08-14 23:30:57 +0800

    添加 toast 和 plugin

---
+++ src/plugin.js
@@ -0,0 +1,15 @@
+import Toast from './toast'
+export default{
+  install(Vue, options){
+    Vue.prototype.$toast = function (message){
+
+      let Constructor = Vue.extend(Toast)
+      let toast = new Constructor()
+      // 上面两句死记硬背
+
+      toast.$slots.default = [message]  // 插槽的文本
+      toast.$mount()  // 必须的，如果不mount toast所有生命周期钩子都不会执行  必须要注意顺序
+      document.body.appendChild(toast.$el) // 把元素放到body中
+    }
+  }
+}
