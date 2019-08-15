f65e884e17f26026f5d0767fd96fab83a1099ad9
li_shuai <506507881@qq.com>
2019-08-16 00:02:23 +0800

    实现了只能有一个 toast 的功能

--- src/plugin.js
+++ src/plugin.js
@@ -1,19 +1,54 @@
 import Toast from './toast'
+
+let currentToast
+
 export default{
   install(Vue, options){
     Vue.prototype.$toast = function (message, toastOptions){
-      function x(){
-        let Constructor = Vue.extend(Toast)
-        let toast = new Constructor({
-          propsData: toastOptions
-        })
-        // 上面两句死记硬背
-  
-        toast.$slots.default = [message]  // 插槽的文本
-        toast.$mount()  // 必须的，如果不mount toast所有生命周期钩子都不会执行  必须要注意顺序
-        document.body.appendChild(toast.$el) // 把元素放到body中
+      if (currentToast) { 
+        currentToast.close()
       }
-      x()  // 新建一个函数
+      currentToast = createToast({Vue, message, propsData: toastOptions})  // 新建一个函数
     }
   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* helpers */
+function createToast({Vue,message,propsData}){
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({propsData})
+  // 上面两句死记硬背
+
+  toast.$slots.default = [message]  // 插槽的文本
+  toast.$mount()  // 必须的，如果不mount toast所有生命周期钩子都不会执行  必须要注意顺序
+  document.body.appendChild(toast.$el) // 把元素放到body中
+  return toast
 }
