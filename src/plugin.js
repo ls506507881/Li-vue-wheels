01b38ac4b4b9f57fdf9e869569c0164a11b8899e
li_shuai <506507881@qq.com>
2019-08-15 23:53:10 +0800

    重构plugin.js

--- src/plugin.js
+++ src/plugin.js
@@ -2,15 +2,18 @@ import Toast from './toast'
 export default{
   install(Vue, options){
     Vue.prototype.$toast = function (message, toastOptions){
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData: toastOptions
-      })
-      // 上面两句死记硬背
-
-      toast.$slots.default = [message]  // 插槽的文本
-      toast.$mount()  // 必须的，如果不mount toast所有生命周期钩子都不会执行  必须要注意顺序
-      document.body.appendChild(toast.$el) // 把元素放到body中
+      function x(){
+        let Constructor = Vue.extend(Toast)
+        let toast = new Constructor({
+          propsData: toastOptions
+        })
+        // 上面两句死记硬背
+  
+        toast.$slots.default = [message]  // 插槽的文本
+        toast.$mount()  // 必须的，如果不mount toast所有生命周期钩子都不会执行  必须要注意顺序
+        document.body.appendChild(toast.$el) // 把元素放到body中
+      }
+      x()  // 新建一个函数
     }
   }
 }
