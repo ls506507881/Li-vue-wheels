7298f7dc19aa63769bb4f515a94e4bd80cbc54b4
li_shuai <506507881@qq.com>
2019-08-21 23:44:38 +0800

    初始化g-popover 组件

---
+++ src/popover.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="popover">
+    <slot name="content"></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GPopover'
+}
+</script>
+
+<style lang="scss" scoped>
+  .popover{
+    display: inline-block;
+    vertical-align: top;
+  }
+</style>
