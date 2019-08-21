cb6c530b11acdf67b45972841f2cf9470b556de7
li_shuai <506507881@qq.com>
2019-08-21 23:56:12 +0800

    创建 popover 组件

--- src/popover.vue
+++ src/popover.vue
@@ -28,6 +28,9 @@ export default {
     position: relative;
     .content-wrapper{
       position: absolute;
+      bottom: 100%;
+      border: 1px solid red;
+      box-shadow: 0 0 3px rgba(0,0,0,0.5)
     }
   }
 </style>
