dbad74cba5ea125b445bdf6e7f27ca854e8d87a5
li_shuai <506507881@qq.com>
2019-08-22 23:03:23 +0800

    解决当前容器有 overflow hidden 造成的 bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'GPopover',
-  data(){
+  data () {
     return {visible: false}
   },
   methods: {
@@ -19,6 +21,13 @@ export default {
       // console.log('切换 visible')
       if(this.visible){
         setTimeout(()=>{
+          document.body.appendChild(this.$refs.contentWrapper)
+          let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
+          // console.log( width, height, top, left)
+          this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+          this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+          // console.log('top',top)
+          // console.log('left',left)
           // console.log('新增 document click 监听器')
           let eventHandler = ()=>{
             // console.log('点击body就关闭')
@@ -27,7 +36,6 @@ export default {
             console.log('document 隐藏 popover')
             document.removeEventListener('click', eventHandler)
           }
-
           document.addEventListener('click', eventHandler)
         },100)
         // this.$nextTick(()=>{
@@ -49,11 +57,11 @@ export default {
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper{
-      position: absolute;
-      bottom: 100%;
-      border: 1px solid red;
-      box-shadow: 0 0 3px rgba(0,0,0,0.5)
-    }
+  }
+  .content-wrapper{
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    transform: translateY(-100%);
   }
 </style>
