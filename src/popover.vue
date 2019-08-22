256302ce5cc9c4614be2543cc4fc36a46d12c59a
li_shuai <506507881@qq.com>
2019-08-23 00:23:29 +0800

    popover 功能方面基本完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click="onClick">
+  <div class="popover" @click="onClick" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
@@ -22,69 +22,41 @@ export default {
       this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
       this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
     },
-    listenToDocument(){
-      let eventHandler = (e)=>{
-        console.log(e.target)
-        if(!this.$refs.contentWrapper.contains(e.target)){
-          this.visible = false
-          document.removeEventListener('click', eventHandler)
-          console.log('关闭')
-        }
-      }
-      document.addEventListener('click', eventHandler)
+    onClickDocument (e){
+      if(this.$refs.popover && 
+      ( this.$refs.popover === e.target ||
+        this.$refs.popover.contains(e.target))
+      ) { return }
+      this.close()
+      console.log('关闭')
     },
-    onShow(){
+    open(){
+      this.visible = true
       // this.$nextTick(()=>{
       //   this.positionContent()
       //   this.listenToDocument()
       // },100)
       setTimeout(()=>{
         this.positionContent()
-        this.listenToDocument()
+        document.addEventListener('click', this.onClickDocument)
       },0)
     },
+    close(){
+      this.visible = false
+      document.removeEventListener('click', this.onClickDocument)
+    },
     onClick (event) {
-      console.log(event.target)
       if(this.$refs.triggerWrapper.contains(event.target)){
         console.log('下面')
-        this.visible = !this.visible
         if(this.visible === true){
-          this.onShow()
+          this.close()
+        } else {
+          this.open()
+          // console.log('关闭')
         }
       }else{
         console.log('上面')
       }
-      // this.visible = !this.visible
-      // // console.log('切换 visible')
-      // if(this.visible){
-      //   setTimeout(()=>{
-      //     document.body.appendChild(this.$refs.contentWrapper)
-      //     let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
-      //     // console.log( width, height, top, left)
-      //     this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-      //     this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-      //     // console.log('top',top)
-      //     // console.log('left',left)
-      //     // console.log('新增 document click 监听器')
-      //     let eventHandler = ()=>{
-      //       // console.log('点击body就关闭')
-      //       this.visible = false
-      //       // console.log('删除监听器')
-      //       // console.log('document 隐藏 popover')
-      //       document.removeEventListener('click', eventHandler)
-      //     }
-      //     document.addEventListener('click', eventHandler)
-      //   },100)
-      //   // this.$nextTick(()=>{
-      //   //   document.addEventListener('click',()=>{
-      //   //     this.visible = false
-      //   //     console.log('点击body就关闭')
-      //   //   })
-      //   // })
-      // }else{
-      //   console.log('关闭')
-      //   // console.log('vm 隐藏 popover')
-      // }
     }
   }
 }
