3270bc03fd60a3207f94dd96c428358e9a84186e
li_shuai <506507881@qq.com>
2019-08-27 00:29:23 +0800

    新建collapse组件

---
+++ src/collapse-item.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="collapseItem">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GCollapseItem"
+}
+</script>
+
+<style lang="stylus" scoped>
+  .collapseItem{
+    
+  }
+</style>
---
+++ src/collapse.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GCollapse"
+}
+</script>
+
+<style lang="stylus" scoped>
+  .collapse{
+
+  }
+</style>
