450b0bcdad2c98f35631dfe9007ba2732d96f387
li_shuai <506507881@qq.com>
2019-08-29 23:59:51 +0800

    完成制作自己的官网

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,17 @@ module.exports = {
   description: '一个正在完善的UI框架',
   themeConfig:{
     sidebar: [
-      '/install/',
-      '/get-started/'
+      {
+        title: '入门',
+        children: [
+          '/install/',
+          '/get-started/',
+        ]
+      },
+      {
+        title: '组件',
+        children: ['/components/button']
+      }
     ]
   }
   
