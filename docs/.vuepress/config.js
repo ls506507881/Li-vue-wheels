935745d349b127315d8e53f08712563495342121
li_shuai <506507881@qq.com>
2019-09-03 00:51:35 +0800

    官网雏形基本建立

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,13 @@
 module.exports = {
+  base: '/li-vue-1/',
   title: 'Li-Vue UI',
   description: '一个正在完善的UI框架',
   themeConfig:{
+    nav: [
+      {text: '主页', link: '/'},
+      {text: '文档', link: '/guide/'},
+      {text: '交流', link: '/https://googles.com'},
+    ],
     sidebar: [
       {
         title: '入门',
@@ -12,7 +18,14 @@ module.exports = {
       },
       {
         title: '组件',
-        children: ['/components/button']
+        children: [
+          '/components/button',
+          '/components/tabs',
+          '/components/input',
+          '/components/grid',
+          '/components/toast',
+          '/components/popover'
+        ]
       }
     ]
   }
